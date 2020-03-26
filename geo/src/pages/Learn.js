2dac36fd884020830b5995b22df8d73b435d88ff
Julia Tsvetaeva <juliats060998@gmail.com>
2020-03-26 20:45:00 +0800

    add map

--- geo/src/pages/Learn.js
+++ geo/src/pages/Learn.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import Head from '../components/Head';
+import { ReactComponent as Map } from '../components/Map.svg';
 
 export default class Learn extends Component {
-	render() {
-		return (
-			<div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: "",
+            password: "",
+            role: "",
+            loginError: ""
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    handleClick(event) {
+        console.log(event.target)
+
+        event.target.style.fill = "#ff0000";
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
 				<Head />
+				<Map onClick={this.handleClick}/>
 			</div>
-		);
-	}
+        );
+    }
 }
--- geo/webpack.config.js
+++ geo/webpack.config.js
@@ -2,21 +2,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const webpack = require('webpack'); //to access built-in plugins
 const htmlFiles = getFilesFromDir(PAGE_DIR, [".html"]);
 
-const htmlPlugins = htmlFiles.map( filePath => {
-  const fileName = filePath.replace(PAGE_DIR, "");
-  return new HtmlWebPackPlugin({
-    chunks:[fileName.replace(path.extname(fileName), ""), "vendor"],
-    template: filePath,
-    filename: fileName})
-});
-
+// const htmlPlugins = htmlFiles.map( filePath => {
+//   const fileName = filePath.replace(PAGE_DIR, "");
+//   return new HtmlWebPackPlugin({
+//     chunks:[fileName.replace(path.extname(fileName), ""), "vendor"],
+//     template: filePath,
+//     filename: fileName})
+// });
 module.exports = {
-  module: {
-    rules: [
-      { test: /\.txt$/, use: 'raw-loader' }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
+    entry: './src/index.js',
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[hash]-[name].[ext]',
+                    },
+                }, ],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './'
+    }
 };
